b520d442fb86c53bd7702b06bd2850c42f6f32b1
AAyar94 <a.ayar94@gmail.com>
2023-01-22 12:11:23 +0300

    pre-release-splash fixed

--- app/src/main/java/com/aayar94/todo/SplashActivity.kt
+++ app/src/main/java/com/aayar94/todo/SplashActivity.kt
@@ -2,24 +2,35 @@ package com.aayar94.todo
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.aayar94.todo.databinding.ActivitySplashBinding
 import java.util.*
 
 
+@Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
-    private lateinit var binding:ActivitySplashBinding
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivitySplashBinding.inflate(layoutInflater)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
         supportActionBar?.hide()
         setContentView(binding.root)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
-        Timer().schedule(object : TimerTask() {
-            override fun run() {
-                startActivity(Intent(applicationContext, MainActivity::class.java))
-            }
-        }, 5000)
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 3000
+        )
+
 
     }
 }
